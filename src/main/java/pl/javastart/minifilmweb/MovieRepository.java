d9e1fc0755e70e62104a5dd4a93638a821c9c2f5
Marcin <victorarmy@gmail.com>
2017-12-08 21:48:42 +0100

    Init commit. changeFilm() unfinished

--- src/main/java/pl/javastart/minifilmweb/MovieRepository.java
+++ src/main/java/pl/javastart/minifilmweb/MovieRepository.java
@@ -3,7 +3,10 @@ package pl.javastart.minifilmweb;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+import javax.transaction.Transactional;
 
+@Repository
+@Transactional
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    Movie findByTitle(String title);
 }
--- src/main/java/pl/javastart/minifilmweb/MovieRestController.java
+++ src/main/java/pl/javastart/minifilmweb/MovieRestController.java
@@ -1,21 +1,75 @@
 package pl.javastart.minifilmweb;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
-@RestController
+@RestController("/api")
 public class MovieRestController {
 
     @Autowired
     private MovieRepository movieRepository;
 
-    @GetMapping("/api/movies")
+    @GetMapping("/movies")
     public List<Movie> getAllMovies() {
         System.out.println("Odpytanie o wszystkie filmy");
         return movieRepository.findAll();
     }
 
+    @GetMapping("/movies/{id}")
+    public ResponseEntity getMovie(@PathVariable long id) {
+        Movie foundMovie = movieRepository.findOne(id);
+        if (foundMovie == null) {
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
+
+        return ResponseEntity.ok(foundMovie);
+    }
+
+    @PostMapping("/movies")
+    public ResponseEntity addNewMovie(@RequestBody Movie newMovie) {
+
+        if (isExistingTheSameMovieInDatabase(newMovie)) {
+            return ResponseEntity
+                    .unprocessableEntity()
+                    .build();
+        }
+
+        Movie savedMovie = movieRepository.save(newMovie);
+        URI uri= getURIForSavedMovie(savedMovie);
+        return ResponseEntity
+                .created(uri)
+                .build();
+    }
+
+    @PutMapping("/movies/{id}")
+    public ResponseEntity changeFilm(@PathVariable long id) {
+        Movie foundMovie = movieRepository.findOne(id);
+        if (foundMovie == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+
+    }
+
+    private boolean isExistingTheSameMovieInDatabase(@RequestBody Movie newMovie) {
+        Movie movieFoundByTheSameTitle = movieRepository.findByTitle(newMovie.getTitle());
+        if (movieFoundByTheSameTitle != null) {
+            return movieFoundByTheSameTitle.equals(newMovie);
+        }
+        return false;
+    }
+
+    private URI getURIForSavedMovie(Movie savedMovie) {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedMovie.getId()).toUri();
+    }
 }
